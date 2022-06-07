e4dd12561d528fa195a356f912a61c4db29478ed
Mike Lester <themikelester@gmail.com>
2022-06-07 23:52:50 +0200

    Parse times from IGC B records

--- src/main.ts
+++ src/main.ts
@@ -65,6 +65,11 @@ class Main {
 
     private parseIGC(text: String) {
         const lines = text.split(/\r\n|\n/);
+
+        const coords = [];
+        const alts = [];
+        const times = [];
+
         for (let i = 0; i < lines.length; i++) {
             const line = lines[i];
             console.log(line);
@@ -74,19 +79,29 @@ class Main {
             // 0 123456 78901234 567890123 4 56789 01234    
             // B 144820 4247731N 00033427W A 00000 02526
             if (line[0] == 'B') {
-                const time = line.substring(1, 7);
+                const timeStr = line.substring(1, 7);
                 const lat = line.substring(7, 15);
                 const lon = line.substring(15, 24);
-
                 const fixValid = line.substring(24, 25) == 'A';
                 const baroAlt = line.substring(25, 30);
                 const gpsAlt = line.substring(30, 37);
 
                 const latValid = lat[7] == 'N' || lat[7] == 'S';
                 const lonValid = lon[8] == 'E' || lon[8] == 'W';
-                assert( latValid && lonValid && fixValid, "Invalid B record found: " + line );
+                assert(latValid && lonValid && fixValid, "Invalid B record found: " + line);
 
-                console.log(time, lat, lon, fixValid, baroAlt, gpsAlt);
+                const time = new Date();
+                time.setUTCHours(
+                    parseInt(timeStr.substring(0, 2)),
+                    parseInt(timeStr.substring(2, 4)),
+                    parseInt(timeStr.substring(4, 6)));
+                times.push(time);
+
+                console.log( 
+                    parseInt(timeStr.substring(0, 2)),
+                    parseInt(timeStr.substring(2, 4)),
+                    parseInt(timeStr.substring(4, 6)));
+                console.log(time, lat, lon, baroAlt, gpsAlt);
             }
         }
     }
